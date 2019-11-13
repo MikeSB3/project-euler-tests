39dea759428fa1a7c0ce1e8985958e0b96fa450c
Student <example@example.com>
2019-11-13 14:56:10 -0800

    complete the first two problems of project euler

---
+++ __test__/multiples.test.js
@@ -0,0 +1,19 @@
+import { remainderCheck, sum, numMultiple, fibonacci, fibSequence, evenAdd } from './../src/main.js';
+// Passed
+// describe ('euler multiples', () => {
+//   test('should sum all multiples of three and five that are below one thousand', () => {
+//   remainderCheck();
+//   expect(sum).toEqual(233168);
+//   expect(numMultiple).toEqual(1000);
+//   });
+// });
+
+// Passed
+// describe ('Even Fibonacci numbers', () => {
+//   test ('create fibonacci sequence that doesnt exceed 4 million', () => {
+//     fibSequence();
+//     evenAdd();
+//     expect(fibonacci).toEqual([1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578])
+//     expect(evenAdd()).toEqual(4613732)
+//   });
+// });
--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+//Passed
+export var sum = 0
+export var numMultiple = 3
+export function remainderCheck() {
+  while (numMultiple < 1000) {
+    if ((numMultiple % 3 === 0) || (numMultiple % 5 === 0)) {
+      sum += numMultiple;
+      numMultiple++;
+    } else {
+      numMultiple++;
+    }
+  };
+};
+// Passed
+export var fibonacci = [1,2]
+export function fibSequence() {
+  for (var i = 2; i <= fibonacci.length; i++){
+    var newNumber = fibonacci[i-1] + fibonacci[i-2]
+    if (newNumber < 4000000) {
+      fibonacci.push(newNumber)
+    } else {
+      break
+    }
+  }
+}
+export function evenAdd() {
+  var total = 0
+  for (var i = 0; i <= fibonacci.length; i++) {
+    if (fibonacci[i] % 2 === 0) {
+      total += fibonacci[i]
+    }
+  }
+  return total
+}
